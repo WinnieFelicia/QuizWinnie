679ed041cd9b7be7575ea93467452f0478486de9
WinnieFelicia <winnie.feliciaa@gmail.com>
2024-02-05 21:13:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/duaduasi/quizwinnie/Activity_profil.java
@@ -0,0 +1,14 @@
+package com.example.duaduasi.quizwinnie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
